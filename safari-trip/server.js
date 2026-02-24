a844d68be54ee404cfefde1ccc73f3874c293fc8
Jordan Maragliano <jordanmaragliano@Jordans-Mac-mini.local>
2026-02-23 20:04:44 -0800

    Safari PDF: force download instead of inline

--- safari-trip/server.js
+++ safari-trip/server.js
@@ -5,7 +5,7 @@ const PORT = 3341;
 app.use(express.static(path.join(__dirname)));
 app.get('/', (req, res) => {
   res.setHeader('Content-Type', 'application/pdf');
-  res.setHeader('Content-Disposition', 'inline; filename="African-Safari-Trip-Plan.pdf"');
+  res.setHeader('Content-Disposition', 'attachment; filename="African-Safari-Trip-Plan.pdf"');
   require('fs').createReadStream(path.join(__dirname, 'safari-trip.pdf')).pipe(res);
 });
 app.listen(PORT, () => console.log(`Safari trip plan running at http://localhost:${PORT}`));
