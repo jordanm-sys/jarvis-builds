7a16b0116683b29a52f264422208ba23f27c8e27
Jordan Maragliano <jordanmaragliano@Jordans-Mac-mini.local>
2026-02-23 19:58:34 -0800

    Remove rogue animal-fact sites that were hogging port 3341

--- animal-fact/server.js
+++
@@ -1,5 +0,0 @@
-const http = require('http');
-const fs = require('fs');
-const path = require('path');
-const html = fs.readFileSync(path.join(__dirname, 'index.html'), 'utf8');
-http.createServer((_, res) => { res.writeHead(200, {'Content-Type':'text/html'}); res.end(html); }).listen(3341, '0.0.0.0', () => console.log('Animal fact site running on http://localhost:3341'));
